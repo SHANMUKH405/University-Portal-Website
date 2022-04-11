efe298ab4360b8c2b04f9abaf70cdd40f62dccf1
Shanmukh Magapu <89508567+SHANMUKH405@users.noreply.github.com>
2022-04-11 19:42:19 +0530

    Add files via upload
    This is  University Portal Website Source Code by Shanmukh Magapu

---
+++ form-handler.php
@@ -0,0 +1,24 @@
+<?php
+$name= $_POST['name'];
+$visitors_email= $_POST['email'];
+$subject= $_POST['subject'];
+$message= $_POST['message'];
+
+$email_from='info@mywebsite.com';
+$email_subject='New Form Submission';
+$email_body="User Name: $name.\n".
+              "User Email: $visitors_email.\n".
+                "Subject: $subject.\n".
+                "User Message:$message.\n";
+$to='shanmukhmagapu@gmail.com';
+$headers="From: $email_from \r\n";
+$headers .="Replt to:$visitors_email\r\n";
+
+
+mail($to,$email_subject,$email_body,$headers);
+header("Location: contact.html");
+
+
+  
+                
+?>
